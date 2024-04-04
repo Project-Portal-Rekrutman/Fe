0083949318a26342900d1c88ecae9b0d64c51ba4
Luqmanhr25 <158405379+Luqmanhr25@users.noreply.github.com>
2024-04-05 05:28:03 +0700

    Create Modal Create Interview Schedule

--- src/components/page/admin/application/index.js
+++ src/components/page/admin/application/index.js
@@ -3,69 +3,84 @@ import axios from "axios"
 import { Button, Card, Col, Container, Row, Table } from "react-bootstrap"
 import { useState, useEffect } from "react";
 import { apiUrl } from "../../../../custom/envcutom.js";
+import ModalInterview from "../../../organism/modal/interview_form"
 const ListApplication = () => {
 
-    const [listData, setListData] = useState([])
+  const [listData, setListData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
 
-    const getData = () => {
-      const config = {
-          headers: {
-              Authorization: localStorage.getItem("user")
-          }
+  const getData = () => {
+    const config = {
+      headers: {
+        Authorization: localStorage.getItem("user")
       }
-      axios.get(`${apiUrl}applies`, config).then((response) => {
-          setListData(response.data.data)
-          console.log(response.data.data);
+    }
+    axios.get(`${apiUrl}applies`, config).then((response) => {
+      setListData(response.data.data)
+      console.log(response.data.data);
 
-      })
+    })
   }
   useEffect(() => {
-      getData();
+    getData();
   }, [])
 
-    return (
-        <>
-            <Container>
-                <Row>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Table  responsive={true} striped={true} borderless={true}>
-                                    <thead>
-                                        <tr>
-                                            <th>Vacancy Title</th>
-                                            <th>Participant Name</th>
-                                            <th>Application Status</th>
-                                            <th>Application Date</th>
-                                            <th>Screening Status</th>
-                                            <th>Screening Date</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {listData.map((value) => (
-                                            <tr key={value.id}>
-                                                <td>{value.vacancy.title}</td>
-                                                <td>{value.participant.user.name}</td>
-                                                <td>{value.appStatus}</td>
-                                                <td>{value.appDate}</td>
-                                                <td>{value.screeningStatus}</td>
-                                                <td>{value.screeningDate}</td>
-                                                <td>
-                                                  <Button style={{marginRight:'20px'}}>Create Interview</Button>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </Table>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+  const getIdApply = (data) => {
+    setSelectedId(data);
+    setShowModal(true);
+  }
 
-        </>
-    )
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedId(null);
+  };
+
+  return (
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Table responsive={true} striped={true} borderless={true}>
+                  <thead>
+                    <tr>
+                      <th>Vacancy Title</th>
+                      <th>Participant Name</th>
+                      <th>Application Status</th>
+                      <th>Application Date</th>
+                      <th>Screening Status</th>
+                      <th>Screening Date</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {listData.map((value) => (
+                      <tr key={value.id}>
+                        <td>{value.vacancy.title}</td>
+                        <td>{value.participant.user.name}</td>
+                        <td>{value.appStatus}</td>
+                        <td>{value.appDate}</td>
+                        <td>{value.screeningStatus}</td>
+                        <td>{value.screeningDate}</td>
+                        <td>
+                          <Button onClick={() => getIdApply(value.id)} style={{ marginRight: '20px' }}>Create Interview</Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <ModalInterview show={showModal} closeModal={closeModal} setShowModal={setShowModal} selectedId={selectedId} />
+      </Container>
+  )
 }
 
 export default ListApplication;
