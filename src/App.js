46fd0e2e06f99a46fc8ab443327fdd5f933acea6
Fajar Rizky <56773371+fajar1411@users.noreply.github.com>
2024-04-04 08:41:45 +0700

    repair apply and create jwt token

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Landing from "./components/page/landing/Index.js"
+import { BrowserRouter, Routes, Route } from "react-router-dom";  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing/>}>
+        {/* <Route index element={<Dashboard/>} />
+        <Route path="demo" element={<Demo/>} /> */}
+        {/* <Route path="contact" element={<Contact />} /> */}
+        {/* <Route path="*" element={<NoPage />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/components/page/landing/Index.js
+++ src/components/page/landing/Index.js
@@ -2,7 +2,7 @@ import "../../../static/bootstrap/css/bootstrap.min.css";
 import "../../../static/assets/owl.carousel/owl.carousel.min.css";
 import "../../../static/assets/owl.carousel/owl.theme.default.css";
 import "../../../static/css/style.css";
-import logo from "../../../static/images/logo-icon.png";
+import logo from "../../../static/images/amartha.PNG";
 import Navbar from "../../organism/navbar/index.js";
 import { Card, Col, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button'
@@ -37,16 +37,7 @@ const Index = () => {
                 </header>
                 <div className="page-wrapper">
                     <div className="container-fluid">
-                        {/* <div className="fix-width"> */}
-                        {/* <div  className="row justify-content-center banner-text">
-                                    <div  className="col-md-10 m-b-20">
-                                        <h1>The Only <span  className="text-info">Admin Template</span> needed for your all <span  className="text-info">Backend Needs</span> Don’t believe? Checkout Yourself</h1>
-                                        <p  className="subtext"><span  className="font-medium">Bootstrap 4 </span>Admin Template + <span  className="font-medium">Angular 2/CLI starter kit</span>, <span  className="font-medium">Light &amp; Dark</span> Versions, Landing Page, <span  className="font-medium">6 Demo</span> Variations, <span  className="font-medium">5 Dashboard</span> Variations, <span  className="font-medium">100+</span> Integrated Plugins, <span  className="font-medium">600+</span> Pages, <span  className="font-medium">3000+</span> Font Icons, <span  className="font-medium">500+</span> UI Components &amp; much more...</p>
-                                        <div  className="down-btn"> <a href="#demos"  className="btn btn-rounded btn-info m-b-10">VIEW DEMOS</a> <a href="https://wrappixel.com/templates/monsteradmin/"  className="btn btn-rounded btn-success m-b-10">BUY NOW</a> </div>
-                                    </div>
-                                </div> */}
-                        {/* <Row className={}></Row> */}
-                        {/* </div> */}
+
                         <div className="row light-blue">
                             <div className="col-md-12" id="demos">
                                 <div className="fix-width">
@@ -54,8 +45,8 @@ const Index = () => {
                                         {applies.map(data => {
 
                                             return (
-                                                <Card  style={{ width: '18rem', margin:'20px 40px 20px 40px',borderRadius: '20px 20px 20px '}}>
-                                                    <Card.Img  variant="top" src={data.image}  style={{ borderRadius: '50px 40px 50px '}}/>
+                                                <Card style={{ width: '18rem', margin: '20px 40px 20px 40px', borderRadius: '20px 20px 20px ' }}>
+                                                    <Card.Img variant="top" src={data.image} style={{ borderRadius: '50px 40px 50px ' }} />
                                                     <Card.Body>
                                                         <Card.Title>{data.title}</Card.Title>
                                                         <Card.Text>
@@ -70,51 +61,16 @@ const Index = () => {
                                 </div>
                             </div>
                         </div>
+                        <footer className="footer row">
+                            <div className="fix-width">
+                                <div className="clearfix"></div>
+                                <div className="col-md-11 sub-footer">
+                                    <span>Copyright 2017. All Rights Reserved by <a className="text-white" href="https://wrappixel.com/templates/monsteradmin/" target="_blank">Monster Admin</a></span>
+                                    <span className="pull-right">Design & Developed by <a className="text-white" href="https://wrappixel.com" target="_blank">WrapPixel</a></span>
 
-
-
-                        {/* <footer  className="footer row">
-                                <div  className="fix-width">
-                                    <div  className="row">
-                                        <div  className="col-lg-3 col-md-6"><img src="images/logo-light-icon.png" alt="logo" /> <img src="images/logo-light-text.png" alt="logo" />
-                                            <p  className="m-t-30">
-                                                <span  className="text-white">Monster Admin</span> is premium quality admin dashboard template with flat design. It is fully responsive admin dashboard template built with Bootstrap Framework, HTML5 & CSS3, Media query. </p>
-                                        </div>
-                                        <div  className="col-lg-3 col-md-6">
-                                            <ul  className="footer-link list-icons">
-                                                <li><a href="../minimal/index.html"><i  className="ti-angle-right"></i> Modern Dashboard</a></li>
-                                                <li><a href="../minimal/index2.html"><i  className="ti-angle-right"></i> Awesome Dashboad</a></li>
-                                                <li><a href="../minimal/index3.html"><i  className="ti-angle-right"></i>  classNamey Dashboad</a></li>
-                                                <li><a href="../minimal/index4.html"><i  className="ti-angle-right"></i> Analytical Dashboard</a></li>
-                                                <li><a href="../minimal/index5.html"><i  className="ti-angle-right"></i> Minimal Dashboard</a></li>
-                                            </ul>
-                                        </div>
-                                        <div  className="col-lg-3 col-md-6">
-                                            <ul  className="footer-link list-icons">
-                                                <li><a href="../minimal/widget-apps.html"><i  className="ti-angle-right"></i> Widget Sections</a></li>
-                                                <li><a href="../minimal/chart-chartist.html"><i  className="ti-angle-right"></i> Chart Options</a></li>
-                                                <li><a href="../minimal/form-layout.html"><i  className="ti-angle-right"></i> Lots of Form Layouts</a></li>
-                                                <li><a href="../minimal/table-data-table.html"><i  className="ti-angle-right"></i> Data Table Options</a></li>
-                                                <li><a href="../minimal/index.html"><i  className="ti-angle-right"></i> Calendar Designs</a></li>
-                                            </ul>
-                                        </div>
-                                        <div  className="col-lg-3 col-md-6">
-                                            <ul  className="footer-link list-icons">
-                                                <li><a href="../minimal/app-email.html"><i  className="ti-angle-right"></i> Inbox Layout</a></li>
-                                                <li><a href="../minimal/pages-treeview.html"><i  className="ti-angle-right"></i> Tree View Options</a></li>
-                                                <li><a href="../minimal/ui-carousel.html"><i  className="ti-angle-right"></i> Carousel Slider Option</a></li>
-                                                <li><a href="../minimal/pages-gallery.html"><i  className="ti-angle-right"></i> Gallery Option</a></li>
-                                                <li><a href="../minimal/starter-kit.html"><i  className="ti-angle-right"></i> Starter Pages</a></li>
-                                            </ul>
-                                        </div>
-                                        <div  className="clearfix"></div>
-                                        <div  className="col-md-12 sub-footer">
-                                            <span>Copyright 2017. All Rights Reserved by <a  className="text-white" href="https://wrappixel.com/templates/monsteradmin/" target="_blank">Monster Admin</a></span>
-                                            <span  className="pull-right">Design & Developed by <a  className="text-white" href="https://wrappixel.com" target="_blank">WrapPixel</a></span>
-                                        </div>
-                                    </div>
                                 </div>
-                            </footer> */}
+                            </div>
+                        </footer>
                     </div>
                 </div>
             </div>
