703fc21f62670f83009a7f6b8d20e376092acecf
Nasri <nasrisarif98@gmail.com>
2024-04-04 08:23:09 +0700

    create modal input score

--- src/components/organism/modal/score/index.js
+++ src/components/organism/modal/score/index.js
@@ -1,9 +1,40 @@
-import "../../../../static/bootstrap/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import axios from 'axios';import "../../../../static/bootstrap/css/bootstrap.min.css";
 import "../../../../static/css/colors/blue.css";
 import "../../../../static/css/style.css";
-import Form from "../../../organism/score";
+import useMessage from "../../../hooks/useMessage";
 
-let index = () => {
+let Index = () => {
+    const [attitude, setAttitude] = useState('');
+    const [communication, setCommunication] = useState('');
+    const [competency, setCompetency] = useState('');
+    const [enthusiasm, setEnthusiasm] = useState('');
+    const [experience, setExperience] = useState('');
+    const [grooming, setGrooming] = useState('');
+
+    const message = useMessage();
+
+    const handleInput = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:8080/api/score", {
+            attitude: attitude,
+            communication: communication,
+            competency: competency,
+            enthusiasm: enthusiasm,
+            experience: experience,
+            grooming: grooming
+        }).then((response) => {
+            message.success(response)
+            console.log(response)
+            // setPassword({ password });
+        }).catch((error) => {
+            message.error(error)
+            console.log(error.response)
+
+        });
+
+    }
+
     return (
         <div className="container-fluid">
             <div className="row">
@@ -98,13 +129,58 @@ let index = () => {
                                     </pre>
                                 </div>
                             </div>
-                            <Form/>
+                            <form className="form">
+                                <div className="form-group m-t-40 row">
+                                    <label for="example-text-input" className="col-2 col-form-label">Attitude</label>
+                                    <div className="col-10">
+                                        <input className="form-control" type="number" required={true} onChange={e => setAttitude(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label for="example-search-input" className="col-2 col-form-label">Communication</label>
+                                    <div className="col-10">
+                                        <input className="form-control" type="number" required={true} onChange={e => setCommunication(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label for="example-email-input" className="col-2 col-form-label">Competency</label>
+                                    <div className="col-10">
+                                        <input className="form-control" type="number" required={true} onChange={e => setCompetency(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label for="example-url-input" className="col-2 col-form-label">Enthusiasm</label>
+                                    <div className="col-10">
+                                        <input className="form-control" type="number" required={true} onChange={e => setEnthusiasm(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label for="example-tel-input" className="col-2 col-form-label">Experience</label>
+                                    <div className="col-10">
+                                        <input className="form-control" type="number" required={true} onChange={e => setExperience(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label for="example-tel-input" className="col-2 col-form-label">Grooming</label>
+                                    <div className="col-10">
+                                        <input className="form-control" type="number" required={true} onChange={e => setGrooming(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="form-group text-center m-t-20">
+                                    <div className="col-xs-12">
+                                        <button className="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit"
+                                            onClick={(e) => handleInput(e)}>SUBMIT</button>
+
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
-export default index;
+export default Index;
