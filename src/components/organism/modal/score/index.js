810900fe50b2a4465923e3952dfb2ae4edd36cf4
Nasri <nasrisarif98@gmail.com>
2024-04-05 05:22:56 +0700

    grading score

--- src/components/organism/modal/score/index.js
+++ src/components/organism/modal/score/index.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';import "../../../../static/bootstrap/css/bootstrap.min.css";
+import axios from 'axios'; import "../../../../static/bootstrap/css/bootstrap.min.css";
 import "../../../../static/css/colors/blue.css";
 import "../../../../static/css/style.css";
 import useMessage from "../../../hooks/useMessage";
+import { Button, ButtonGroup, Form, Modal } from "react-bootstrap";
 
-let Index = () => {
+let Index = (props) => {
     const [attitude, setAttitude] = useState('');
     const [communication, setCommunication] = useState('');
     const [competency, setCompetency] = useState('');
@@ -23,7 +24,7 @@ let Index = () => {
             enthusiasm: enthusiasm,
             experience: experience,
             grooming: grooming,
-            interview_id : 10
+            interview_id: props.id
         }).then((response) => {
             message.success(response)
             console.log(response)
@@ -38,72 +39,65 @@ let Index = () => {
 
     return (
 
-        <Modal show={props.show} onHide={props.closeModal} size={"lg"} backdrop={"static"}> 
+        <Modal show={props.show} onHide={props.closeModal} size={"lg"} backdrop={"static"}>
 
             <Modal.Body>
-            <div className="card">
-                        <div className="card-body">
-                            <button className="btn btn-danger btn-sm pull-right collapsed" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> Get code <i className="ti-angle-down"></i></button>
-                            <h4 className="card-title">Input Score</h4>
-                            <h6 className="card-subtitle"> All bootstrap element classNameies </h6>
-                            <form className="form">
-                                <div className="form-group m-t-40 row">
-                                    <label for="example-text-input" className="col-2 col-form-label">Attitude</label>
-                                    <div className="col-10">
-                                        <input className="form-control" type="number" required={true} onChange={e => setAttitude(e.target.value)} />
-                                    </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h4 className="card-title">Input Score</h4>
+                        <h6 className="card-subtitle"> All bootstrap element classNameies </h6>
+                        <form className="form">
+                            <div className="form-group m-t-40 row">
+                                <label for="example-text-input" className="col-2 col-form-label">Attitude</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="number" required={true} onChange={e => setAttitude(e.target.value)} />
                                 </div>
-                                <div className="form-group row">
-                                    <label for="example-search-input" className="col-2 col-form-label">Communication</label>
-                                    <div className="col-10">
-                                        <input className="form-control" type="number" required={true} onChange={e => setCommunication(e.target.value)} />
-                                    </div>
+                            </div>
+                            <div className="form-group row">
+                                <label for="example-search-input" className="col-2 col-form-label">Communication</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="number" required={true} onChange={e => setCommunication(e.target.value)} />
                                 </div>
-                                <div className="form-group row">
-                                    <label for="example-email-input" className="col-2 col-form-label">Competency</label>
-                                    <div className="col-10">
-                                        <input className="form-control" type="number" required={true} onChange={e => setCompetency(e.target.value)} />
-                                    </div>
+                            </div>
+                            <div className="form-group row">
+                                <label for="example-email-input" className="col-2 col-form-label">Competency</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="number" required={true} onChange={e => setCompetency(e.target.value)} />
                                 </div>
-                                <div className="form-group row">
-                                    <label for="example-url-input" className="col-2 col-form-label">Enthusiasm</label>
-                                    <div className="col-10">
-                                        <input className="form-control" type="number" required={true} onChange={e => setEnthusiasm(e.target.value)} />
-                                    </div>
+                            </div>
+                            <div className="form-group row">
+                                <label for="example-url-input" className="col-2 col-form-label">Enthusiasm</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="number" required={true} onChange={e => setEnthusiasm(e.target.value)} />
                                 </div>
-                                <div className="form-group row">
-                                    <label for="example-tel-input" className="col-2 col-form-label">Experience</label>
-                                    <div className="col-10">
-                                        <input className="form-control" type="number" required={true} onChange={e => setExperience(e.target.value)} />
-                                    </div>
+                            </div>
+                            <div className="form-group row">
+                                <label for="example-tel-input" className="col-2 col-form-label">Experience</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="number" required={true} onChange={e => setExperience(e.target.value)} />
                                 </div>
-                                <div className="form-group row">
-                                    <label for="example-tel-input" className="col-2 col-form-label">Grooming</label>
-                                    <div className="col-10">
-                                        <input className="form-control" type="number" required={true} onChange={e => setGrooming(e.target.value)} />
-                                    </div>
+                            </div>
+                            <div className="form-group row">
+                                <label for="example-tel-input" className="col-2 col-form-label">Grooming</label>
+                                <div className="col-10">
+                                    <input className="form-control" type="number" required={true} onChange={e => setGrooming(e.target.value)} />
                                 </div>
-                                <div className="form-group text-center m-t-20">
-                                    <div className="col-xs-12">
-                                        <button className="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit"
-                                            onClick={(e) => handleInput(e)}>SUBMIT</button>
-
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
+                </div>
             </Modal.Body>
             <Modal.Footer>
                 <ButtonGroup >
-                <Button onClick={props.closeModal} style={{ marginRight: '10px' }}>CLOSE</Button>
-                {/* <Button onClick={handleSubmit}>Send</Button>  */}
+                    <Button onClick={props.closeModal} style={{ marginRight: '10px' }}>CLOSE</Button>
+                    <Button onClick={(e) => handleInput(e)}>SAVE</Button>
+                    {/* <Button onClick={handleSubmit}>Send</Button>  */}
                 </ButtonGroup>
 
             </Modal.Footer>
         </Modal>
-        
-       
+
+
 
     )
 }
